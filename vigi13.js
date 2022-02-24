20a08431c79263891870fef0494bb9d35c277fcb
Raimondas Knystautas <email@com.com>
2022-02-24 20:46:36 +0200

    Pakoreguotas vartotojo svoris

--- vigi13.js
+++ vigi13.js
@@ -1,8 +1,10 @@
-const vartotojas = { 
+const vartotojas = {
     vardas: "Marius",
     svoris: 90,
     ugis: 191,
 };
 
 vartotojas.pinigine = 1000;
+vartotojas.svoris = 60;
+
 console.log(vartotojas);
