10b9e39850dbe8f0032944ad77748dcec87bf1bf
Tomas Uzpurvis <tomas605@gmail.com>
2022-02-24 21:29:30 +0200

    Pasisveikinimas

--- vigi13.js
+++ vigi13.js
@@ -2,15 +2,19 @@ const vartotojas = {
     vardas: "Marius",
     svoris: 90,
     ugis: 191,
+    pasisveikinti: function () {
+        return "Labas as " + this.vardas;
+    }
 };
 
 vartotojas.pinigine = 1000;
 vartotojas.svoris = 60;
 
+const pasisveikinimas = vartotojas.pasisveikinti();
 
+console.log(pasisveikinimas);
 const savybe = prompt('kokia savybe');
 const verte = prompt('Apibudinkite pasirinkta savybe');
-
 
 vartotojas[savybe] = verte;
 
