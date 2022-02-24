a06941bee4fbbeffcb48b45f641d2472592258da
Tadas Simokaitis <tadassimokaitis@yahoo.com>
2022-02-24 21:10:35 +0200

    Davem galimybe

--- vigi13.js
+++ vigi13.js
@@ -7,4 +7,11 @@ const vartotojas = {
 vartotojas.pinigine = 1000;
 vartotojas.svoris = 60;
 
+
+const savybe = prompt('kokia savybe');
+const verte = prompt('Apibudinkite pasirinkta savybe');
+
+
+vartotojas[savybe] = verte;
+
 console.log(vartotojas);
